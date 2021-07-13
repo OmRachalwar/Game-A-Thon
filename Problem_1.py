7316cf6baf6b69a9a39067e06c00d0ef62df7e0b
Om Rachalwar <69252015+OmRachalwar@users.noreply.github.com>
2021-07-13 23:40:00 +0530

    initial commit

--- Problem_1.py
+++ Problem_1.py
@@ -1,7 +1,13 @@
 def array123(nums):
+  
   if len(nums)>2:
+    
     for i in range(len(nums)-2):
+      
       if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+        
         return True
+    
     return False
+  
   return False
